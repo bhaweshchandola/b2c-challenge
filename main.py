04dba1498e6c3d8aed6346b6c0265995f2fddf17
bhaweshchandola <bhawesh.chandola@gmail.com>
2019-09-08 18:11:14 +0530

    structure change

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 import json
 import math
-cust = open("Customers _Assignment_Coding Challenge.txt", 'r')
-fir = cust.readline()
-jfir = json.loads(fir)
-print(type(fir))
-print(type(jfir))
-import pprint
-pprint.pprint(jfir)
-print(jfir["user_id"])
 
+dub_lat,dub_lon = (53.339428, -6.257664)
 
 def deg2rad(deg):
-    return deg * (math.pi/180)
+    return float(deg) * (math.pi/180)
 
 def distanceCal(lat1, lon1, lat2, lon2):
+
+    lat2 = float(lat2)
+    lon2 = float(lon2)
     R = 6371 # radius of earth
     dlat = deg2rad(lat2-lat1)
     dlon = deg2rad(lon2-lon1)
@@ -26,4 +22,23 @@ def distanceCal(lat1, lon1, lat2, lon2):
     d = R * c
     return d
 
-print(distanceCal(53.339428, -6.257664, 52.986375, -6.043701))
+
+cust = open("Customers _Assignment_Coding Challenge.txt", 'r')
+fir = cust.readlines()
+# jfir = json.loads(fir)
+# print(type(fir))
+# print(type(jfir))
+# import pprint
+# pprint.pprint(jfir)
+# print(jfir["user_id"])
+
+for x in fir:
+    x = json.loads(x)
+    dist = round(distanceCal(dub_lat, dub_lon, x["latitude"], x["longitude"]),2)
+
+    print("user_id: {}, distance: {}".format(x["user_id"],dist))
+
+
+
+
+# print(distanceCal(53.339428, -6.257664, 52.986375, -6.043701))
